ae7d0797086430d5d2e7c22de31ab6089e54622e
Jos. Araya <marijose.259@gmail.com>
2025-10-30 20:07:47 -0600

    .

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,43 @@
 export default {
-   transform: {
-    "^.+\\.js$": "babel-jest",
-  },
   testEnvironment: "node",
   verbose: true,
   testMatch: ["**/tests/**/*.test.js"],
+  
+  // 🔑 NO uses transform con ESM
+  transform: {},
+  
+  // 🔑 CRÍTICO: Permite que Jest procese mongodb y sus dependencias
+  transformIgnorePatterns: [
+    "node_modules/(?!(" +
+    "mongodb|" +
+    "mongoose|" +
+    "bson|" +
+    "@mongodb-js|" +
+    "socks" +
+    ")/)"
+  ],
+  
+  // 🔑 Mapeo de módulos para ESM
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  
+  // 🔑 Resolver extensiones
+  moduleFileExtensions: ["js", "json", "node"],
+  
+  // Setup files
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
+  
+  // 🔑 AUMENTAR timeout para las operaciones de Mongoose
+  testTimeout: 15000,
+  
+  // 🔑 CRÍTICO: Forzar cierre después de las pruebas
+  forceExit: true,
+  
+  // 🔑 Detectar handles abiertos para debugging
+  detectOpenHandles: true,
+  
+  // Reportes
   reporters: [
     "default",
     [
@@ -19,5 +52,4 @@ export default {
       },
     ],
   ],
-
 };
